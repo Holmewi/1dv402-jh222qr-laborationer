346cbed572b89004fb559154a52a138df14dc9e3
Joakim Holmewi <jh222qr@student.lnu.se>
2014-09-10 00:01:36 +0200

    Skrev kommentarer för att få en överblick över problemet

---
+++ Lab1-Change/Lab1-Change/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_Change
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarera variabler
+
+
+            // Input från användaren
+
+
+            // Räkna ut öresavrundning
+
+
+            // Räkna ut differens
+
+
+            // Skriv ut kvittens
+
+
+            // Räkna ut sedlar tillbaka
+
+
+            // Skriv ut sedlar tillbaka
+
+
+            // Felmeddelanden        
+        }
+    }
+}
