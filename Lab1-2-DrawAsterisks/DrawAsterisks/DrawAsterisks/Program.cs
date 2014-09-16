6075a4d986780e714d9981ffed688a3840ef947c
Joakim Holmewi <jh222qr@student.lnu.se>
2014-09-16 16:06:37 +0200

    Created the project for laboration 1-2 and listed the problem feed

---
+++ Lab1-2-DrawAsterisks/DrawAsterisks/DrawAsterisks/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrawAsterisks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarera variabler
+            
+
+            // Satser
+            Console.Write(" ");
+            Console.Write("* ");
+            Console.WriteLine();
+
+            // Skapa nästlade for-loopar med asterisker innhållande 39 kolumner och 25 rader
+
+
+            // Skjut in varannan rad med hjälp av en if-sats och modulusoperator
+
+
+            // Skapa modulusoperatorn för att färglägga raderna med hjälp av en switch-satss
+
+
+
+        }
+    }
+}
