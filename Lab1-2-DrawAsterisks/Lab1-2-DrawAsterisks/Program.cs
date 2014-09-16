64ae2ff706e824b1ca739fd56794cfe48013bcc7
Joakim Holmewi <jh222qr@student.lnu.se>
2014-09-17 00:17:00 +0200

    Created the if statement for pushing the odd line with a modulos operator. Created the switch to color the lines with help of the modulos operator

--- Lab1-2-DrawAsterisks/Lab1-2-DrawAsterisks/Program.cs
+++ Lab1-2-DrawAsterisks/Lab1-2-DrawAsterisks/Program.cs
@@ -10,48 +10,49 @@ namespace DrawAsterisks
     {
         static void Main(string[] args)
         {
-            // Deklarera variabler
-
-
-
             // Skapa nästlade for-loopar med asterisker innhållande 39 kolumner och 25 rader
-
-            /*
-            for (int col = 0; col < 25; col++ )
-            { 
-                for (int i = 0; i < 39; i++)
-                {
-                    Console.Write("* ");
-                    for (int j = 0; j < 0; j++)
-                    {
-                        Console.Write(" ");
-                    }
-                }
-                Console.WriteLine("");
-            }
-            */
 
             for (int row = 0; row < 25; row++)
             {
+                // Skjut in varannan rad med hjälp av en if-sats och modulusoperator
+                // Hittade lite hjälp på källan http://www.dotnetperls.com/odd
+                // Om parameten row inte är delbar med 2 så är remindern inte lika med 0, vilket tyder på att talet inte är jämt
+
+                if (row % 2 != 0)
+                {  
+                    Console.Write(" ");
+                }
                 for (int col = 0; col < 39; col++)
                 {
+                    // Skapa modulusoperatorn för att färglägga raderna med hjälp av en switch-sats
+                    switch (row % 3)
+                    {
+                        case 0:
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            break;
+
+                        case 1:
+                            Console.ForegroundColor = ConsoleColor.Magenta;
+                            break;
+
+                        case 2:
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            break;
+                    }
+
                     Console.Write("* ");
-                        for (int j = 0; j < 0; j++)
-                        {
-                            Console.Write(" ");
-                        }
+                    Console.ResetColor();
                 }
+
                 Console.WriteLine("");
             }
 
+            
 
-            // Skjut in varannan rad med hjälp av en if-sats och modulusoperator
-
-
-            // Skapa modulusoperatorn för att färglägga raderna med hjälp av en switch-satss
-
-
+            
 
         }
+
+
     }
 }
