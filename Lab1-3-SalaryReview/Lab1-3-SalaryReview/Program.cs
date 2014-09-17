8abb46f5358626e8279186329e1cfd883a71a97e
Joakim Holmewi <jh222qr@student.lnu.se>
2014-09-17 11:55:26 +0200

    Created project for lab 1-3 and listed the problem feed with comments

---
+++ Lab1-3-SalaryReview/Lab1-3-SalaryReview/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1_3_SalaryReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarera variabler
+
+
+            // Mata in antal löner som ska redovisas - skriv ut meddelande och värdet
+
+
+            // Mata in löner och spara dessa i en array av typen int - skrev ut meddelande och värdet
+
+
+            // Räkna ut medianlön - Sortera arrayen och plocka ut medianlönen
+
+
+            // Räkna ut medellön - Addera lönerna och dividera med antalet inmatade löner
+
+
+            // Räkna ut lönespridning - Subrahera den högsta lönen med den lägsta
+
+
+            // Skapa en felhantering med try-catch-satser
+
+
+            // Skapa hantering av key input där escape avslutar programmet medan andra tangenter startar om applikationen
+
+
+        }
+    }
+}
