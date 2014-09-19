0a2e8c2d8e41b79db46288085f3d00e35c12fa93
Joakim Holmewi <jh222qr@student.lnu.se>
2014-09-19 17:43:42 +0200

    Created the user input for amount of salaries listed in an array and the unput for each listed salary

--- Lab1-3-SalaryReview/Lab1-3-SalaryReview/Program.cs
+++ Lab1-3-SalaryReview/Lab1-3-SalaryReview/Program.cs
@@ -11,14 +11,24 @@ namespace Lab1_3_SalaryReview
         static void Main(string[] args)
         {
             // Deklarera variabler
-
+            int _salaryCount;
+            int _salary = 0;
 
             // Mata in antal löner som ska redovisas - skriv ut meddelande och värdet
+            Console.Write("Ange antal löner att mata in: ");
+            _salaryCount = int.Parse(Console.ReadLine());
 
 
-            // Mata in löner och spara dessa i en array av typen int - skrev ut meddelande och värdet
+            // Mata in löner och spara dessa i en array av typen int - skriv ut meddelande och värdet
+
+            for (int row = 1; row <= _salaryCount; row++)
+            {
+                Console.Write("Ange lön nummer {0}: ", row);
+                _salary = int.Parse(Console.ReadLine());
+            }
 
 
+            
             // Räkna ut medianlön - Sortera arrayen och plocka ut medianlönen
 
 
@@ -35,5 +45,7 @@ namespace Lab1_3_SalaryReview
 
 
         }
+
+
     }
 }
