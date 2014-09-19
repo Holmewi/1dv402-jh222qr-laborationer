5bfd5e46c2b793adf757b940e9455be029a19bdb
Joakim Holmewi <jh222qr@student.lnu.se>
2014-09-20 01:19:57 +0200

    Created the sorting of the medium salary in a new method

--- Lab1-3-SalaryReview/Lab1-3-SalaryReview/Program.cs
+++ Lab1-3-SalaryReview/Lab1-3-SalaryReview/Program.cs
@@ -29,35 +29,57 @@ namespace Lab1_3_SalaryReview
                 _salary[row] = int.Parse(Console.ReadLine());
             }
 
-
+            Console.WriteLine("\n-----------------------------------");
+            MediumSalary(_salary, _salaryCount, row);
+            Console.WriteLine("-----------------------------------\n");
             
             for (row = 0; row < _salaryCount; row++ )
             {
                 Console.WriteLine(_salary[row]);
             }
 
-
-
-
-
-            
-            // Räkna ut medianlön - Sortera arrayen och plocka ut medianlönen
-
-
-            // Räkna ut medellön - Addera lönerna och dividera med antalet inmatade löner
-
-
-            // Räkna ut lönespridning - Subrahera den högsta lönen med den lägsta
-
-
-            // Skapa en felhantering med try-catch-satser
-
-
-            // Skapa hantering av key input där escape avslutar programmet medan andra tangenter startar om applikationen
-
-
         }
 
+        // Räkna ut medianlön - Sortera arrayen och plocka ut medianlönen
+
+        /* Jag satt själv och kodade ihop en liknande lösning genom att dra av -1 på slutvärder på variabel 'b'
+         * men tog för givet att det måste finnas ett bättre sätt. Men till min förvåning och glädje var
+         * det första jag hittade en liknande lösning.
+         * Källa: http://stackoverflow.com/questions/5275115/add-a-median-method-to-a-list
+         */
+        static void MediumSalary(int[] _salary, int _salaryCount, int row)
+        {
+            Array.Sort(_salary);
+            int _count = _salary.Length;           
+           
+            if (_count % 2 == 0)
+            {
+                int a = _salary[_count / 2 - 1];
+                int b = _salary[_count / 2];
+                Console.WriteLine("Medianlön: {0}", (a + b) / 2);
+            }
+            else
+            {
+                Console.WriteLine("Medianlön: {0}", _salary[_count / 2]);
+            }
+        }
+
+        // Räkna ut medellön - Addera lönerna och dividera med antalet inmatade löner
+        static void AvarageSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Räkna ut lönespridning - Subrahera den högsta lönen med den lägsta
+        static void SalaryDistribution()
+        {
+            throw new NotImplementedException();
+        }
+
+        // Skapa en felhantering med try-catch-satser
+
+
+        // Skapa hantering av key input där escape avslutar programmet medan andra tangenter startar om applikationen
 
 
     }
