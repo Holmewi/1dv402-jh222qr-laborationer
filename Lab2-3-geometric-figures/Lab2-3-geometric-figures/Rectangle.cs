6e6278ca3ae5c0cbc5e776ab4370a5eaef29108e
Joakim Holmewi <jh222qr@student.lnu.se>
2014-10-15 14:34:30 +0200

    Created the classes Shape, Ellipse and Rectangle

---
+++ Lab2-3-geometric-figures/Lab2-3-geometric-figures/Rectangle.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_3_geometric_figures
+{
+    class Rectangle : Shape
+    {
+
+    }
+}
