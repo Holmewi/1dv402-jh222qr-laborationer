d714fd85dcc1e6f3b8647575105f13b66db03113
Joakim Holmewi <jh222qr@student.lnu.se>
2014-10-15 15:17:19 +0200

    Created the fields, properties and methods in the class Shape

--- Lab2-3-geometric-figures/Lab2-3-geometric-figures/Shape.cs
+++ Lab2-3-geometric-figures/Lab2-3-geometric-figures/Shape.cs
@@ -8,6 +8,45 @@ namespace Lab2_3_geometric_figures
 {
     public abstract class Shape
     {
-        
+        // Fields
+        private double _length;
+        private double _width;
+
+        // Properties
+        public abstract double Area
+        {
+            get { return 1.5; }     // Temporary return values
+        }
+
+        public double Length
+        {
+            get { return 2.5; }     // Temporary return values
+
+            set { }
+        }
+
+        public abstract double Perimeter
+        {
+            get { return 1.5; }    // Temporary return values
+        }
+
+        public double Width
+        {
+            get { return 2.5; }     // Temporary return values
+
+            set { }
+        }
+
+        // Constructor
+        protected Shape (double lenght, double width)
+        {
+
+        }
+
+        // Method
+        public string ToString()
+        {
+            return "Temp";          // Temporary return values
+        }
     }
 }
